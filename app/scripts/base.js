9c5568300f68cf3cfcf89a45fd00dcc19ec4f04b
wxw <dreamoflose@gmail.com>
2016-05-10 16:52:37 +0800

    基本风水设置

---
+++ app/scripts/base.js
@@ -0,0 +1,5 @@
+/**
+ * @description Administrator
+ * @class Administrator
+ * @author yongjin<zjut_wyj@163.com> 2016/5/10
+ */
---
+++ app/scripts/status/status.js
@@ -0,0 +1,11 @@
+/**
+ * @description status.js
+ * @class status.js
+ * @author wu xiao wei <836790625@qq.com> 2016/5/10
+ */
+var book=[
+  {name:'javascript指导', text:'基础js代码'},
+  {name:'javascript指导1', text:'基础js代码'},
+  {name:'javascript指导2', text:'基础js代码'},
+  {name:'javascript指导3', text:'基础js代码'}
+];
