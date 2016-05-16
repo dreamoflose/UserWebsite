0fcbcddb79015ab7be43dd94fd285ccc8d27a817
wxw <dreamoflose@gmail.com>
2016-05-16 17:56:40 +0800

    加入react框架 -- 测试中

---
+++ app/scripts/control/Control.js
@@ -0,0 +1,5 @@
+/**
+ * @description Control.js
+ * @class Control.js
+ * @author 吴小伟(shevy) <836790625@qq.com> 2016/5/16 14:03
+ */
---
+++ app/scripts/service/Service.js
@@ -0,0 +1,5 @@
+/**
+ * @description Service.js
+ * @class Service.js
+ * @author 吴小伟(shevy) <836790625@qq.com> 2016/5/16 15:42
+ */
